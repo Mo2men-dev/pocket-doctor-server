a522d3d0156086317038eb123e45cd3ed7f2460e
Mo2men-dev <momensameh5@gmail.com>
2024-06-29 14:44:48 +0300

    damn cors

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import mediator from './src/handlers/mediators';
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOptions = {
-  origin: '*',
+  origin: `${process.env.CLIENT_URL}`,
   optionsSuccessStatus: 200
 };
 
