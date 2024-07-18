eacadbdc6d448a22496f29bc8f50b8ba379879d4
Mo2men-dev <momensameh5@gmail.com>
2024-07-18 15:40:58 +0300

    improvment: added diff env for dev and testing

--- src/database/database.ts
+++ src/database/database.ts
@@ -5,19 +5,34 @@ dotenv.config();
 const ENV = process.env.ENV;
 let client: Pool;
 
+if (ENV === 'prod') {
+  client = new Pool({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    port: 5432,
+    ssl: true
+  });
+}
+
 if (ENV === 'dev') {
   client = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'pockect_doctor_dev',
+    port: 5432
   });
 }
 
 if (ENV === 'test') {
   client = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    host: 'localhost',
+    user: 'postgres',
+    password: 'postgres',
+    database: 'pockect_doctor_test',
+    port: 5432
   });
 }
 
